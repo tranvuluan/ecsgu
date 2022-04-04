ad4d9b221c1646dde303e2ce042354ac1d9d45f9
Nguyễn Đức Mạnh <67572602+xekomonh241@users.noreply.github.com>
2022-04-04 17:49:07 +0700

    nhacungcap

---
+++ admin/assets/js/supplier.js
@@ -0,0 +1,132 @@
+function viewToAdd() {
+
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            viewToAdd: true
+        },
+        
+        success: function(data) {
+            $('#switchModal').html($('<div class="modal fade">' +data+' <div>').modal());
+        }
+    });
+}
+
+function add() {
+    event.preventDefault();
+    let id = $('input[name="add_Id"]').val();
+    let address = $('input[name="add_address"]').val();
+    let name = $('input[name="add_name"]').val();
+
+
+    // $('#switchModal').html($('<div class="modal fade">' +data+' <div>').modal());
+
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            id: id,
+            address: address,
+            name: name,
+            add : true
+        },
+        success: function(response) {
+            console.log(response);
+            if(response == 0){
+                console.log(response);
+            }
+            else{
+                window.location.href = "./nhacungcap.php";
+            }
+        }
+    });
+}
+
+function getDetail(id) {
+    console.log(id);
+
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            id: id,
+            view: true
+        },
+        
+        success: function(data) {
+            $('#switchModal').html($('<div class="modal fade">' +data+' <div>').modal());
+        }
+    });
+}
+
+
+function viewToUpdate(id) {
+    console.log(id);
+
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            id: id,
+            viewToUpdate: true
+        },
+        success: function(data) {
+            $('#switchModal').html($('<div class="modal fade">' +data+' <div>').modal());
+        }
+    });
+}
+
+function update() {
+    event.preventDefault();
+    let id = $('input[name="update_Id"]').val();
+    let address = $('input[name="update_address"]').val();
+    let name = $('input[name="update_name"]').val();
+
+    var data = 
+    // $('#switchModal').html($('<div class="modal fade">' +data+' <div>').modal());
+
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            id: id,
+            address: address,
+            name: name,
+            update : true
+        },
+        success: function(response) {
+            console.log(response);
+            if(response == 0){
+                console.log(response);
+            }
+            else{
+                window.location.href = "./nhacungcap.php";
+            }
+        }
+    });
+}
+
+function deleteVoucher(id){
+
+    console.log(id);
+    $.ajax({
+        url: './process/supplier-process.php',
+        type: 'POST',
+        data: {
+            id: id,
+            delete: true
+        },
+        success: function(response) {
+            console.log(response);
+            if(response == 0){
+                console.log(response);
+                alert('Lỗi');
+            }
+            else{
+                alert('Xóa thành công');
+                location.reload();
+            }
+        }
+    });
+}
