6a25661568e891778b55f865645b358db3ebe88d
Parikun1412 <justinbk1412@gmail.com>
2022-04-06 13:16:17 +0700

    xxx
    xx

--- admin/process/employee.php
+++ admin/process/employee.php
@@ -1,6 +1,9 @@
 <?php
 $path = dirname(__FILE__);
 require_once $path . '/../../class/employee.php';
+require_once $path . '/../class/position.php';
+require_once $path . '/../class/account.php';
+
 ?>
 
 <?php
@@ -12,74 +15,93 @@ if (isset($_POST['viewToAdd'])) {
                 <div class="card-body">
                     <h6 class="mb-0">Thông tin nhân viên</h6>
                     <div class="p-4 border rounded">
-                        <form class="row g-3 needs-validation" novalidate>
+                        <form class="row g-3 needs-validation" method="POST" onsubmit="add()">
                             <div class="col-md-3">
                                 <label for="validationCustom01" class="form-label">Mã nhân viên</label>
-                                <input type="text" class="form-control" id="validationCustom01" value="" required>
+                                <input type="text" class="form-control" id="validationCustom01" name="employeeId" value="EM<?php echo (int) (microtime(true) * 1000) ?>" required>
                             </div>
 
                             <div class="col-md-3">
                                 <label for="validationCustom03" class="form-label">Giới tính</label>
-                                <select class="form-select" aria-label="Default select example">
-                                    <option value="1">Nam</option>
-                                    <option value="2">Nữ</option>
-                                    <option value="3">Khác</option>
+                                <select name="gender" class="form-select" aria-label="Default select example">
+                                    <option value="Nam">Nam</option>
+                                    <option value="Nữ">Nữ</option>
                                 </select>
                             </div>
                             <div class="col-md-3">
                                 <label class="form-label">Ngày sinh</label>
-                                <input type="text" class="form-control datepicker" />
+                                <input type="text" class="form-control datepicker" name="birthday" value="" required>
                             </div>
                             <div class="col-md-3">
-                                <label for="validationCustom04" class="form-label">CMND/CCCD</label>
-                                <input type="text" class="form-control" id="validationCustom03" required>
+                                <label for="validationCustom04" class="form-label">CMND</label>
+                                <input type="text" class="form-control" id="validationCustom03" name="cmnd" value="" required>
                             </div>
                             <div class="col-md-6">
                                 <label for="validationCustom02" class="form-label">Tên nhân viên</label>
-                                <input type="text" class="form-control" id="validationCustom02" value="" required>
+                                <input type="text" class="form-control" id="validationCustom02" name="employeeName" value="" required>
                             </div>
                             <div class="col-md-6">
                                 <label for="validationCustom04" class="form-label">Email</label>
-                                <input type="text" class="form-control" id="validationCustom03" required>
+                                <input type="text" class="form-control" id="validationCustom03" name="email" value="" required>
                             </div>
                             <div class="col-md-8">
                                 <label for="validationCustom04" class="form-label">Địa chỉ</label>
-                                <input type="text" class="form-control" id="validationCustom03" required>
+                                <input type="text" class="form-control" id="validationCustom03" name="address" value="" required>
                             </div>
                             <div class="col-md-4">
                                 <label for="validationCustom04" class="form-label">Số điện thoại</label>
-                                <input type="text" class="form-control" id="validationCustom03" required>
+                                <input type="text" class="form-control" id="validationCustom03" name="phone" value="" required>
                             </div>
 
                             <div class="col-md-6">
                                 <label for="validationCustom03" class="form-label">Chức vụ</label>
-                                <select class="form-select col-md-2" aria-label="Default select example">
-                                    <option value="1">Quản lý</option>
-                                    <option value="2">Nhân viên</option>
+                                <select name="position" class="form-select col-md-2" aria-label="Default select example">
+                                    <?php
+                                    $positionModel = new Position();
+
+                                    $getPosition = $positionModel->getPositions();
+                                    if ($getPosition) {
+                                        while ($rowPosition = $getPosition->fetch_assoc()) {
+                                    ?>
+
+                                            <option value="<?php echo $rowPosition['id_position'] ?>"><?php echo $rowPosition['name'] ?></option>
+
+                                    <?php
+                                        }
+                                    }
+                                    ?>
                                 </select>
                             </div>
-                            <div class="col-md-6">
-                                <label for="validationCustom04" class="form-label">Tài khoản</label>
-                                <input type="text" class="form-control" id="validationCustom03" required>
-                            </div>
-
+                            <div class="col-md-6"></div>
                             <div class="col-md-6">
                                 <label for="validationCustom04" class="form-label">Trạng thái</label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                    <input class="form-check-input" type="radio" name="active" value="1" id="flexRadioDefault1">
                                     <label class="form-check-label" for="flexRadioDefault1">Hoạt động</label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked="">
+                                    <input class="form-check-input" type="radio" name="lock" value="0" id="flexRadioDefault2" checked="">
                                     <label class="form-check-label" for="flexRadioDefault2">Khóa</label>
                                 </div>
                             </div>
+                            <div class="col-md-6"></div>
+
+                            <div class="col-md-6">
+                                <label for="validationCustom04" class="form-label">Tài khoản</label>
+                                <input type="text" class="form-control" name="username" value="" required>
+                            </div>
+                            <div class="col-md-6"></div>
                             <div class="col-md-6">
                                 <label for="validationCustom04" class="form-label">Mật khẩu</label>
-                                <input type="password" class="form-control" id="validationCustom03" required>
+                                <input type="password" class="form-control" name="password" value="" required>
+                            </div>
+                            <div class="col-md-6"></div>
+                            <div class="col-md-6">
+                                <label for="validationCustom04" class="form-label">Xác nhận mật khẩu</label>
+                                <input type="password" class="form-control" name="confirm_password" value="" required>
                             </div>
                             <div class="col-12">
-                                <button class="btn btn-primary" type="submit">Submit form</button>
+                                <button class="btn btn-primary" type="submit">Thêm</button>
                             </div>
                         </form>
                     </div>
@@ -89,4 +111,39 @@ if (isset($_POST['viewToAdd'])) {
     </div>
 <?php
 }
+?>
+
+<?php
+
+    if(isset($_POST['add'])){
+
+        $employeeId = $_POST['id'];
+        $gender = $_POST['gender'];
+        $birthday = $_POST['birthday'];
+        $cmnd = $_POST['cmnd'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $position = $_POST['position'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+
+        $accountModel = new Account();
+        if($password == $confirm_password){
+            $addAccount = $accountModel->insert($employeeId, $username, $password)
+        }
+        
+
+        $employeeModel = new Employee();
+            $addEmployee = $employeeModel->insert($employeeId, $position, $name, $gender, $birthday, $phone, $email, $address,'abc', $cmnd);
+            if($addEmployee){
+                echo 'add thanh cong';
+            }else{
+                echo 'and fail';
+            }
+        
+    }
+
 ?>
