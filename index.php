548f5b3bb4391be8a3adf4a489f0ae4144eb0f27
Nguyễn Đức Mạnh <67572602+xekomonh241@users.noreply.github.com>
2022-05-01 14:13:16 +0700

    hh

--- index.php
+++ index.php
@@ -253,11 +253,11 @@ require_once($path . '/class/product.php')
                 <!-- Tab Start -->
                 <div class="col-lg-auto col-md-auto col-12">
                     <ul class="product-tab-nav nav">
-                        <li class="nav-item"><a class="nav-link active" data-bs-toggle="tab" href="#tab-product-all">All</a></li>
-                        <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#tab-product-new">New</a>
+                        <li class="nav-item"><a class="nav-link active" onclick="viewProductAll()" href="#tab-product-all">All</a></li>
+                        <li class="nav-item"><a class="nav-link" onclick="viewProductNew()" href="#tab-product-new">New</a>
                         </li>
-                        <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#tab-product-bestsellers">Bestsellers</a></li>
-                        <li class="nav-item"><a class="nav-link" data-bs-toggle="tab" href="#tab-product-itemssale">Items
+                        <li class="nav-item"><a class="nav-link" onclick="viewProductBestSellers()" href="#tab-product-bestsellers">Bestsellers</a></li>
+                        <li class="nav-item"><a class="nav-link" onclick="viewProductSale()" href="#tab-product-itemssale">Items
                                 Sale</a></li>
                     </ul>
                 </div>
@@ -279,7 +279,7 @@ require_once($path . '/class/product.php')
                                             <!-- Single Prodect -->
                                             <div class="product">
                                                 <div class="thumb">
-                                                    <a href="<?php echo 'product-details.php?id_product=' . $row['id_product'] ?>"  class="image">
+                                                    <a href="<?php echo 'product-details.php?id_product=' . $row['id_product'] ?>" class="image">
                                                         <img src="<?php echo $row['image'] ?>" alt="Product" />
                                                         <img class="hover-image" src="<?php echo $row['image'] ?>" alt="Product" />
                                                     </a>
@@ -318,29 +318,29 @@ require_once($path . '/class/product.php')
 
                         <!-- 2st tab start -->
                         <div class="tab-pane fade show" id="tab-product-new">
+                            
                             <div class="row">
-                                <?php $showproduct2 = $productModel->getProducts();
-                                if ($showproduct2) {
-                                    while ($row2 = $showproduct2->fetch_assoc()) {
+                                <?php $showproduct = $productModel->getProducts();
+                                if ($showproduct) {
+                                    while ($row = $showproduct->fetch_assoc()) {
                                 ?>
                                         <div class="col-lg-4 col-xl-3 col-md-6 col-sm-6 col-xs-6 mb-30px" data-aos="fade-up" data-aos-delay="200">
                                             <!-- Single Prodect -->
                                             <div class="product">
                                                 <div class="thumb">
-                                                    <a href="product-details.php" class="image">
-                                                        <img src="<?php echo $row2['image'] ?>" alt="Product" />
-                                                        <img class="hover-image" src="<?php echo $row2['image'] ?>" alt="Product" />
+                                                    <a href="<?php echo 'product-details.php?id_product=' . $row['id_product'] ?>" class="image">
+                                                        <img src="<?php echo $row['image'] ?>" alt="Product" />
+                                                        <img class="hover-image" src="<?php echo $row['image'] ?>" alt="Product" />
                                                     </a>
                                                     <span class="badges">
                                                         <span class="new">New</span>
                                                     </span>
                                                     <div class="actions">
-                                                        <a href="wishlist.html" class="action wishlist" title="Wishlist"><i class="pe-7s-like"></i></a>
-                                                        <a href="#" class="action quickview" data-link-action="quickview" title="Quick view" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="pe-7s-search"></i></a>
-                                                        <a href="compare.html" class="action compare" title="Compare"><i class="pe-7s-refresh-2"></i></a>
+                                                        <a href="wishlist.php" class="action wishlist" title="Wishlist"><i class="pe-7s-like"></i></a>
+                                                        <a href="#" onclick="viewDetailModal()" class="action quickview" data-link-action="quickview" title="Quick view" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="pe-7s-search"></i></a>
                                                     </div>
-                                                    <button title="Add To Cart" class=" add-to-cart">Add
-                                                        To Cart</button>
+                                                    <!-- <a href="#" onclick="addToCart('<?php print $row['id_product'] ?>')" title="Add To Cart" class=" add-to-cart">Add
+                                                        To Cart</a> -->
                                                 </div>
                                                 <div class="content">
                                                     <span class="ratings">
@@ -349,11 +349,11 @@ require_once($path . '/class/product.php')
                                                         </span>
                                                         <span class="rating-num">( 5 Review )</span>
                                                     </span>
-                                                    <h5 class="title"><a href="product-details.php"><?php echo $row2['name'] ?>
+                                                    <h5 class="title"><a href="product-details.php"><?php echo $row['name'] ?>
                                                         </a>
                                                     </h5>
                                                     <span class="price">
-                                                        <span class="new"><?php echo $row2['price'] ?></span>
+                                                        <span class="new"><?php echo $row['price'] ?></span>
                                                     </span>
                                                 </div>
                                             </div>
@@ -362,7 +362,6 @@ require_once($path . '/class/product.php')
                                     }
                                 }
                                 ?>
-
                             </div>
                         </div>
                         <!-- 2nd tab end -->
@@ -692,7 +691,7 @@ require_once($path . '/class/product.php')
     $path = dirname(__FILE__);
     require_once($path . '/includes/scripts.php') ?>
     <!-- END JavaScripts -->
-                       
+
     <script src="./assets/js/app.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
